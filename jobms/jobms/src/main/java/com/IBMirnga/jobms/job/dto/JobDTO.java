92f944f5354dd58b6d9d56d02d3464ce1eef9366
Ibrahim Mirnga <ibrahim234mohammed@gmail.com>
2025-02-24 11:54:46 +0100

    Microservices hard try

---
+++ jobms/jobms/src/main/java/com/IBMirnga/jobms/job/dto/JobDTO.java
@@ -0,0 +1,23 @@
+package com.IBMirnga.jobms.job.dto;
+
+import com.IBMirnga.jobms.job.external.Company;
+import com.IBMirnga.jobms.job.external.Review;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class JobDTO {
+
+    private Long Id;
+    private String title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+    private Company company;
+    private Review review;
+
+}
